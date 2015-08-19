f2d517d5f9f772186ca2cf8356a906cad72770d2
mtnjoroge <mtnjoroge@gmail.com>
2015-08-19 12:37:49 +0300

    fibonacci & sequence

--- src/Working.java
+++ src/Working.java
@@ -3,8 +3,20 @@ public class Working {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      System.out.println(divisors(25, 3));
-	}
+		String Str = new String("Martin Nganga");
+//      System.out.println(multiadd(2, 3));
+//      System.out.println(Str.substring(1, 5));
+//		for(int i = Str.length() - 1; i >= 0; i--){
+//			System.out.print(Str.charAt(i));
+			int i = 1;
+			while (i <= 6) {
+			System.out.print(2*i + " ");
+			i = i + 1;
+			}
+			System.out.println("");
+			
+		}
+	
 	
 	public static int fibonacci(int x){
 		if(x == 0 || x == 1){
@@ -16,6 +28,13 @@ public class Working {
 	
 	public static boolean divisors(int x, int y){
 		return (x % y == 0);
+	}
+	
+	// xe −x + 1 − e −x
+	
+	public static double multiadd(double x, double e){
+		double result = x * Math.exp(-x) + Math.pow((1 - Math.exp(-x)), 0.5);
+		return result;
 	}
 
 }
