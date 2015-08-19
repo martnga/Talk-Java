0f8f0a193102afba4622958bafc2f49d8d1ade53
mtnjoroge <mtnjoroge@gmail.com>
2015-08-19 17:32:25 +0300

    incremental Character count

--- src/Working.java
+++ src/Working.java
@@ -3,13 +3,21 @@ public class Working {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String Str = new String("Martin Nganga");
+		String fruit = new String("banana");
 //      System.out.println(multiadd(2, 3));
 //      System.out.println(Str.substring(1, 5));
 //		for(int i = Str.length() - 1; i >= 0; i--){
-//			System.out.print(Str.charAt(i));
-		Printtable(5);
-			
+//		System.out.print(Str.charAt(i));
+//      Printtable(5);
+		int index = 0;
+		int count = 0;
+		while (index < fruit.length()) {
+		if (fruit.charAt(index) == 'a' ) {
+		count++;
+		}
+		index++;
+		}
+		System.out.println(count);
 		}
 	
 	public static boolean divisors(int x, int y){
