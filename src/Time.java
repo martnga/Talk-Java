9f844c14a3806cdaa68508cae7629216a05a0273
mtnjoroge <mtnjoroge@gmail.com>
2015-08-21 11:54:44 +0300

    printing object in a human readable way

--- src/Time.java
+++ src/Time.java
@@ -9,6 +9,7 @@ public class Time {
 		this.second = 0.0;
 	}
 	
+	//example of overloading
 	public Time(int hour, int minute, double second){
 		this.hour = hour;
 		this.minute = minute;
@@ -18,7 +19,21 @@ public class Time {
 		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Time t1 = new Time();
+		t1.hour = 11;
+		t1.minute = 8;
+		t1.second = 3.14159;
+		 printTime(t1);  //if you print objects directly you get object type and a unique hexadecimal code
+		
+		// another way to do the same thing
+		Time t2 = new Time(11, 8, 3.14159);
+		 printTime(t2);
 
+	}
+	
+	public static void printTime(Time t){
+		System.out.println(t.hour + ":" + t.minute + ":"  + t.second);
 	}
 
 }
