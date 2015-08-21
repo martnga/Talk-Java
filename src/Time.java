c814c921ea9819880e2305fd6dc4f883573983ec
mtnjoroge <mtnjoroge@gmail.com>
2015-08-21 11:41:36 +0300

    multiple constructors solving for different arguments during object instantion

--- src/Time.java
+++ src/Time.java
@@ -1,6 +1,21 @@
 
 public class Time {
-
+	int hour, minute;
+	double second;
+	
+	public Time(){
+		this.hour = 0;
+		this.minute = 0;
+		this.second = 0.0;
+	}
+	
+	public Time(int hour, int minute, double second){
+		this.hour = hour;
+		this.minute = minute;
+		this.second = second;
+	}
+			 
+		
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
